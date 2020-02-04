1defe1b72b761c4f8de918fb7f7ed67af8bf10bc
Kym Richardson <kym.richardson@auckland.ac.nz>
2020-02-04 13:14:07 +1300

    done by kym
    done by kym

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello World - KYM WAS HERE"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
